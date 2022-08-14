e56b56b2aa0b263eef91bc58fcd9edd2a492496b
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-08-14 14:40:21 +0000

    Add

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+
+
+  has_one_attached :profile_image
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('')
+      profile_image.attach(io: File.open(file_path), filename: '', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+
+  resources :books, only: [:new, :index, :show]
+  resources :users, only:[:new, :show, :index]
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
