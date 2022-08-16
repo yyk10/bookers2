57c778a60f54f9606c1b7e1a3a3e65d1ec0840ea
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-08-16 13:37:17 +0000

    Uodate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,18 @@
 class BooksController < ApplicationController
   def new
+     @book = Book.new
   end
 
+ def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to 'book_path'
+  end
   def index
+
   end
 
   def show
