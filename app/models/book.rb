df3cb117b7cc4f5e4bad1849130c659101cd897b
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-09-02 15:55:46 +0000

    Uodate

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
    validates :body, presence: true
    validates :title, presence: true
    validates :body, length: {maximum: 200}
-  
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
     #validates :name,:introduction,presence: true
     validates :introduction, length: {maximum: 50}
     validates :name, length: {minimum:2,maximum: 20}
-  
-     
+
+
   has_many :books, dependent: :destroy
 
 
   has_one_attached :profile_image
-  
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
